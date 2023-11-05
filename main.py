e01354a3a93589dbecdccb770cdbd61790be9297
Dgarzonac <123409139+Dgarzonac@users.noreply.github.com>
2023-11-05 16:48:13 -0500

    calculadora

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 root = Tk()
 root.title("Calculadora")
 root.resizable(0,0)
-root.geometry("400x350")
+root.geometry("300x250")
 
 def clickBoton_0():
     texto_actual = pantalla.get()
@@ -87,7 +87,7 @@ def clickBoton_igual():
 
 # Configuración pantalla de salida 
 pantalla = Entry(root, width=40, bg="black", fg="white", borderwidth=0, font=("arial", 18, "bold"))
-pantalla.grid(row=0, column=0, columnspan=8, padx=1, pady=1)
+pantalla.grid(row=0, column=0, columnspan=55, padx=1, pady=1)
 
 # Configuración botones
 boton_1 = Button(root, text="1", command = clickBoton_1, width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=1, column=0, padx=1, pady=1)
@@ -100,7 +100,7 @@ boton_7 = Button(root, text="7", command = clickBoton_7, width=9, height=3, bg="
 boton_8 = Button(root, text="8", command = clickBoton_8, width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=3, column=1, padx=1, pady=1)
 boton_9 = Button(root, text="9", command = clickBoton_9, width=9, height=3, bg="white", fg="red", borderwidth=0, cursor="hand2").grid(row=3, column=2, padx=1, pady=1)
 boton_igual = Button(root, text="=", command = clickBoton_igual, width=20, height=3, bg="red", fg="white", borderwidth=0, cursor=" hand2").grid(row=4, column=0, columnspan=2, padx=1, pady=1)
-boton_punto = Button(root, text=".", command = clickBoton_punto, width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0).grid(row=4, column=1, padx=1, pady=1)
+boton_punto = Button(root, text=".", command = clickBoton_punto, width=9, height=3, bg="spring green", fg="black", cursor="hand2", borderwidth=0).grid(row=4, column=2, padx=1, pady=1)
 boton_mas = Button(root, text="+", command = clickBoton_mas, width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=1, column=3, padx=1, pady=1)
 boton_menos = Button(root, text="-", command = clickBoton_menos , width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=2, column=3, padx=1, pady=1)
 boton_multiplicacion = Button(root, text="*", command = clickBoton_multiplicacion,  width=9, height=3, bg="deep sky blue", fg="black", borderwidth=0, cursor="hand2").grid(row=3, column=3, padx=1, pady=1)
